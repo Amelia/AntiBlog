3a83de9d02df4d75d5549d02f1b60d3132dab7cb
Joseph Nguyen <chocobo13132000@yahoo.com>
2011-04-15 17:01:14 -0400

    CreateAccount Form is up. will work on other frontend Stuff

--- createAccount.php
+++ createAccount.php
@@ -8,9 +8,40 @@
 	<?php
 		include 'head.php';
 	?>
+
+    <div id="page"><div class="inner_copy"><div class="inner_copy">Best selection of premium <a href="http://www.templatemonster.com/pack/joomla-1-6-templates/">Joomla 1.6 templates</a></div></div>
+		<div id="page-bgtop">
+			<div id="page-bgbtm">
+				<div class="post">
+					<h2 class="title">Please provide us with the following information in order to create your account. A unique username is required.</h2>
+					<p class="meta"><span class="date">&nbsp;</span><span class="posted">&nbsp;</span></p>
+					<div style="clear:both">&nbsp;</div>
+						<div class="entry">
+							<form method="post" action="registerAccount.php">
+                            <label for="username">Username:</label>
+                            <input type="text" id="username" name="username" /><br />
+                            <label for="pw">Password:</label>
+                            <input type="password" id="pw" name="pw" /><br /><p></p>
+                            <table>
+                            <tr>
+                            <td><input type="submit" value="Register" name="submit" /></td>
+                            </table>
+                            </form>
+						</div>
+					</div>
+					<div style="clear:both">&nbsp;</div>
+				</div>
+			</div>
+		</div>
+
+<h1></h1>
+
+
+
+
 	<div>
-		Account Creation
-	</div>
+<body>
+
 </div>
 </body>
 </html>
