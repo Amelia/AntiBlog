3cbd6477f8d5f621eb542c5436116e7a820e44cd
Phillip Thomas <pthomas@rosemary.umw.edu>
2011-04-20 14:44:49 -0400

    Cleaned code in registerAccount.php

--- registerAccount.php
+++ registerAccount.php
@@ -9,32 +9,30 @@
 <div id="wrapper">
 	<?php
 		include 'createAccountHead.php';
-        include "anti_xss.php";
+   		include "anti_xss.php";
 
-    $name = anti_xss($_POST['username']);
-    $pw = $_POST['pw'];
+    		$name = anti_xss($_POST['username']);
+    		$pw = $_POST['pw'];
 
-//    $name=mysqli_real_escape_string($db,$name);
-//    $pw=mysqli_real_escape_string($db,$pw);
+		//    $name=mysqli_real_escape_string($db,$name);
+		//    $pw=mysqli_real_escape_string($db,$pw);
 
-    $connection = new Mongo();
-    $db = $connection->antiblog;
-    $collection = $db->users;
+    		$connection = new Mongo();
+    		$db = $connection->antiblog;
+    		$collection = $db->users;
 
-    $cursor = $collection->find();
-    $error = FALSE;
-    foreach($cursor as $obj){
-        if($obj["username"] == $name){
-            $error = TRUE;
-        }
-    }
-    if($error == FALSE){
-        $obj= array("username"=>$name, "password" => $pw);
-        $collection->insert($obj);
-    }
-
-
-    ?>
+    		$cursor = $collection->find();
+    		$error = FALSE;
+    		foreach($cursor as $obj){
+        		if($obj["username"] == $name){
+            			$error = TRUE;
+        		}
+    		}
+    		if($error == FALSE){
+        		$obj= array("username"=>$name, "password" => $pw);
+        		$collection->insert($obj);
+   	 	}
+	?>
 
     <div id="page"><div class="inner_copy"><div class="inner_copy">Best selection of premium <a href="http://www.templatemonster.com/pack/joomla-1-6-templates/">Joomla 1.6 templates</a></div></div>
 		<div id="page-bgtop">
