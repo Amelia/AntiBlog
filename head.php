b55adace42060d9afdf006eef844d0a53e72932c
Phillip Thomas <pthomas@rosemary.umw.edu>
2011-04-20 20:20:56 -0400

    Moved the greetings for logged in user.

--- head.php
+++ head.php
@@ -9,26 +9,29 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 			<h1><a href="index.php">anti-blog </a></h1>
 			<p><i>let's talk</i></p>
 		</div>
-		<?php if($_SESSION['name'] == '' )
-		{
-		?>
 		<div id="login">
 			<form action="login.php" method="post" id="login" onsubmit="return formValidate('login','username', 'password')">
 				<table>
+					<?php if($_SESSION['name'] == '' )
+						{
+					?>
 					<tr><td>user name:</td>
 						<td><input id="username" type="text" name="username"></td>
 					<tr><td>password:</td>
 						<td><input id="password" type="password" name="pw"></td>
 					<tr><td><a href="createAccount.php">register?</a></td>
 						<td><button id="Login">login</button></td>
+					<?php
+						}else{
+							echo "<br/><br/><br/><br/>";
+							
+
+							echo "<td>Welcome, ", $_SESSION['name'], "</td>";
+						}
+					?>
 				</table>
 			</form>
 		</div>
-		<?php
-		}
-		else
-			echo "Welcome, ",$_SESSION['name'];
-		?>
 	</div>
 	<div id="menu">
 		<ul>
