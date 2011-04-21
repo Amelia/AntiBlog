5cd41fa0c33ee52efbfb416ef15e9a239643a48a
Phillip Thomas <pthomas@rosemary.umw.edu>
2011-04-20 21:21:49 -0400

    Cleaned up the Create Account

--- createAccount.php
+++ createAccount.php
@@ -7,27 +7,31 @@
 <div id="wrapper">
 	<?php
 		include 'createAccountHead.php';
-//    phpinfo();
 	?>
 
     <div id="page"><div class="inner_copy"><div class="inner_copy">Best selection of premium <a href="http://www.templatemonster.com/pack/joomla-1-6-templates/">Joomla 1.6 templates</a></div></div>
 		<div id="page-bgtop">
 			<div id="page-bgbtm">
 				<div class="post">
-					<h2 class="title">Please provide us with the following information in order to create your account. A unique username is required.</h2>
-					<p class="meta"><span class="date">&nbsp;</span><span class="posted">&nbsp;</span></p>
+					<h2 class="title">Create Account A unique username is required.</h2>
+					<p class="meta"><span class="date">Requirements: &nbsp; -Unique Username &nbsp; -Six digit Username &nbsp; -Six digit Password &nbsp;</span><span class="posted">&nbsp;</span></p>
 					<div style="clear:both">&nbsp;</div>
 						<div class="entry">
 							<script type="text/javascript" language="JavaScript" src="scripts/formValidate.js"></script>
 							<form method="post" id="create" action="registerAccount.php" onsubmit="return formValidate('create', 'username', 'password', 'confirmation')">
-								<label for="username">Username:</label>
-								<input type="text" id="username" name="username" /><br />
-								<label for="password">Password:</label>
-								<input type="password" id="password" name="password" /><br />
-								<label for="confirmation">Confirmation:</label>
-								<input type="password" id="confirmation" name="confirmation" /><br /><p></p>
-
-								<input type="submit" value="Register" name="submit" />
+								<table>
+										<td><label for="username">Username:</label></td>
+										<td><input type="text" id="username" name="username" /></td>
+									<tr>
+										<td><label for="password">Password:</label></td>
+										<td><input type="password" id="password" name="password" /></td></tr>
+									<tr>
+										<td><label for="confirmation">Confirmation:</label></td>
+										<td><input type="password" id="confirmation" name="confirmation" /></td></tr>
+									<tr>
+										<td></td>
+										<td><input type="submit" value="Register" name="submit" /></td></tr>
+								</table>
 							</form>
 						</div>
 					</div>
