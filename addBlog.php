b1a130bfcb7deba4fad11b66e423b6017bed6686
Michael Glitzos <mglitzos@rosemary.umw.edu>
2011-04-21 09:58:34 -0400

    Another blanking txt field

--- addBlog.php
+++ addBlog.php
@@ -34,7 +34,7 @@ else{
 		</script>
                 <form action=addBlogController.php method=post name=addBlog>
 				<div class="post">
-					<h2 class="title"><input type="text" name="title" size=100 value="Untitled" style='width:100%;'></h2>
+					<h2 class="title"><input type="text" name="title" size=100 value="Untitled" style='width:100%;' id="txtit" onfocus="clearText('txtit')"></h2>
 					<p class="meta"><span class="date">(Date/time will auto-set when you submit.)</span><span class="posted">Posted by: <?php echo $_SESSION['name'] ?></span></p>							
 					<div style="clear:both">&nbsp;</div>
 						<div class="entry">
